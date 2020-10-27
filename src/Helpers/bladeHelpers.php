d4c455dbc30ea73bda11048bb784b839a8741c9d
Uğur AKÇIL <ugurakcil@gmail.com>
2020-10-27 11:42:47 +0300

    added search in collection helper

--- src/Helpers/bladeHelpers.php
+++ src/Helpers/bladeHelpers.php
@@ -13,11 +13,22 @@ if (! function_exists('localizedDate')) {
         $newDate = new Carbon\Carbon;
         echo $newDate::parse($date)->formatLocalized($format);
     }
-
 }
 
 if (! function_exists('helperCheck')) {
     function helperCheck() {
         return 'Helpers working';
+    }
+}
+
+if(! function_exists('hasCategoryInArray')) {
+    function hasCategoryInArray($records, $categoryField, $exceptedValue) {
+        foreach($records as $record) {
+            if($record[$categoryField] == $exceptedValue) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }

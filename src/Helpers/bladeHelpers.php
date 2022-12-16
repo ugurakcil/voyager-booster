29bacaae27d4f76bdc1ba5baec162cb166e7165d
ugurakcil <ugurakcil@gmail.com>
2022-12-16 16:09:21 +0300

    strtittle

--- src/Helpers/bladeHelpers.php
+++ src/Helpers/bladeHelpers.php
@@ -42,16 +42,26 @@ if(! function_exists('afterImageName')) {
 if(! function_exists('strupper')) {
     function strupper($text){
         if(app()->getLocale() == 'tr')
-            $text = str_replace('i','İ',$text);
+            return Transliterator::create('tr-Upper')->transliterate($text);
 
         return mb_convert_case($text, MB_CASE_UPPER, "UTF-8");
     }
 }
+
 if(! function_exists('strlower')) {
     function strlower($text){
         if(app()->getLocale() == 'tr')
-            $text = str_replace('İ','i',$text);
+            return Transliterator::create('tr-Lower')->transliterate($text);
 
         return mb_convert_case($text, MB_CASE_LOWER, "UTF-8");
+    }
+}
+
+if(! function_exists('strtitle')) {
+    function strtitle($text){
+        if(app()->getLocale() == 'tr')
+            return Transliterator::create('tr-Title')->transliterate($text);
+
+        return mb_convert_case($text, MB_CASE_TITLE, "UTF-8");
     }
 }
